006d031d2b3e2a9dea4f8167446a468d5e7e6a82
machie38 <michaelnakis01@gmail.com>
2023-09-04 12:37:08 -0400

    Latest from Lesson 26

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 import React from 'react'
+// import { render } from 'react-dom';
 import ReactDOM from 'react-dom/client'
-import './index.scss'
+
 import { BrowserRouter } from 'react-router-dom'
+
 import App from './App'
 import { UserProvider } from './contexts/user.context'
-// import { ProductsProvider } from './contexts/products.context'
+import { CategoriesProvider } from './contexts/categories.context'
 import { CartProvider } from './contexts/cart.context'
-import reportWebVitals from './reportWebVitals'
 
+import './index.scss'
+
+// const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <UserProvider>
-                <CartProvider>
-                    <App />
-                </CartProvider>
+                <CategoriesProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </CategoriesProvider>
             </UserProvider>
         </BrowserRouter>
     </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
